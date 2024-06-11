61ed69c143d4cff81a8664cbf4d63880dd84f7ad
Kamil Chodoła <43241881+kamilchodola@users.noreply.github.com>
2024-06-11 17:17:10 +0200

    Update index.js

--- .github/actions/linode-machine-manager/index.js
+++ .github/actions/linode-machine-manager/index.js
@@ -2,6 +2,9 @@ const core = require('@actions/core');
 const { LinodeClient } = require('@linode/api-v4');
 const { execSync } = require('child_process');
 const axios = require('axios');
+const { execSync } = require('child_process');
+
+execSync('npm install', { stdio: 'inherit' });
 
 async function run() {
   try {
