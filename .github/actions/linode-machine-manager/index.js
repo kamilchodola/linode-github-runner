9582b488aad4433be8fea51fb45c06b8728fea25
Kamil Chodoła <43241881+kamilchodola@users.noreply.github.com>
2024-06-12 08:34:10 +0200

    Update index.js

--- .github/actions/linode-machine-manager/index.js
+++ .github/actions/linode-machine-manager/index.js
@@ -94,6 +94,7 @@ async function run() {
 
       const registrationToken = registrationTokenResponse.data.token;
       const runnerScript = `
+        export RUNNER_ALLOW_RUNASROOT="1"
         mkdir actions-runner && cd actions-runner
         curl -o actions-runner-linux-x64-2.284.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.284.0/actions-runner-linux-x64-2.284.0.tar.gz
         tar xzf ./actions-runner-linux-x64-2.284.0.tar.gz
